179dcacea2ccecd898033368ec65ee06597012a9
Kiril Gourinov <kikoiakia@gmail.com>
2022-01-09 23:59:25 +0200

    Updated recipe button

--- scripts.js
+++ scripts.js
@@ -1,47 +1,54 @@
 const api_url = `https://api.npoint.io/51ed846bdd74ff693d7e`;
-async function MakeTable(url) {
+
+
+async function GetContent(url) {
     const response = await fetch(url);
     var data = await response.json();
     if (response) {
-
-        const dataLength = data.meals.length;
-        var htmlText = ``;
-        for(var i = 0; i < dataLength; i++){                        
-            const name = data.meals[i].name;
-            const image = data.meals[i].image;
-            const region = data.meals[i].region;
-            const category = data.meals[i].category;
-            const ingredients = data.meals[i].ingredients;
-            const instruction = data.meals[i].instruction;
-            if(i % 2 == 0){
-                const content = `<div class="mealsOdd">\n <img class="recipePic" src="`+image +
-                    `">\n <p class="mealName">`+name+
-                        `</p>\n <div class="regionAndCategory">\n <p class="mealRegionCategory"> <span id="category">`+category+
-                            `</span>, <span id="region">`+region+`</span></p>\n</div>\n<button class="seeRecipe">See Recipe</button>\n</div>`
-                htmlText += content;
-                
-            }
-            else{
-                const content = `<div class="mealsEven">\n <img class="recipePic" src="`+image +
-                    `">\n <p class="mealName">`+name+
-                        `</p>\n <div class="regionAndCategory">\n <p class="mealRegionCategory"> <span id="category">`+category+
-                            `</span>, <span id="region">`+region+`</span></p>\n</div>\n<button class="seeRecipe">See Recipe</button>\n</div>`
-                htmlText += content;
-                
-            }
-        }
-        var el = document.getElementById("meals");
-        el.innerHTML = htmlText;
         var tempData = {
             categories : data.categories,
             meals : data.meals,
             regions : data.regions
         }
+        generateMeals(tempData);
         return tempData;
     }
 }
 
-var APIContent = MakeTable(api_url).then(result => APIContent = result);
+var APIContent = GetContent(api_url).then(result => APIContent = result);
+
+
+function generateMeals(data){
+    var htmlText = "";
+    var length = data.meals.length;
+    for(var i = 0; i < length; i++){     
+        const id = data.meals[i].id;  
+        const name = data.meals[i].name;
+        const image = data.meals[i].image;
+        const region = data.meals[i].region;
+        const category = data.meals[i].category;
+        if(i % 2 == 0){
+            const content = `<div class="mealsOdd">\n <img class="recipePic" src="`+image +
+                `">\n <p class="mealName">`+name+
+                    `</p>\n <div class="regionAndCategory">\n <p class="mealRegionCategory"> <span id="category">`+category+
+                        `</span>, <span id="region">`+region+`</span></p>\n</div>\n<button class="seeRecipe" onclick="showRecipe(this)" id="`+id+`">See Recipe</button>\n</div>`;
+            htmlText += content;
+            
+        }
+        else{
+            const content = `<div class="mealsEven">\n <img class="recipePic" src="`+image +
+                `">\n <p class="mealName">`+name+
+                    `</p>\n <div class="regionAndCategory">\n <p class="mealRegionCategory"> <span id="category">`+category+
+                    `</span>, <span id="region">`+region+`</span></p>\n</div>\n<button class="seeRecipe" onclick="showRecipe(this)" id="`+id+`">See Recipe</button>\n</div>`;
+            htmlText += content;
+            
+        }
+    }
+    var el = document.getElementById("meals");
+    el.innerHTML = htmlText;
+    
+}
+
 
 function FilterName() {
     var input, filter, ul, li, a, i, txtValue;
@@ -49,8 +56,8 @@ function FilterName() {
     filter = input.value.toUpperCase();
     ul = document.getElementById("meals");
     li = ul.getElementsByTagName('div');
-    for (i = 0; i < li.length; i ++) {
-        a = li[i].getElementsByTagName(`p`).namedItem("mealName");
+    for (i = 0; i < li.length; i+=2) {
+        a = li[i].getElementsByTagName(`p`)[0];
         txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
@@ -94,5 +101,43 @@ function FilterCategory() {
             li[i].style.display = "none";
         }
     }
+}
+
+function showRecipe(elem){
+    var recipe = APIContent.meals.find(data => (data.id == elem.id));
+    var htmlText = "";
+    var tableText = "";
+    var x = document.createElement("table");
+    var header = x.createTHead();
+    var row = header.insertRow();
+    var cell = row.insertCell(0);
+    cell.innerHTML = `Ingredients`;
+    var cell2 = row.insertCell(1);
+    cell2.innerHTML = `Measure`;
+    var length = recipe.ingredients.length;
+    for(var i = 0; i < length; i++){
+        var ingredients = x.insertRow();
+        var cell3 = ingredients.insertCell(0);
+        cell3.innerHTML = recipe.ingredients[i].name;
+        var cell4 = ingredients.insertCell(1);
+        cell4.innerHTML = recipe.ingredients[i].measure;
+    }
+    tableText = `<table id="table_id">` + x.innerHTML + `</table>`;
+    const content = `\n<div class="modal" id="modal">\n<div class="modalHeader">\n<p class="modalTitle">`+recipe.name+`</p>\n</div>\n<div class="modalBody">\n<img class="modalPic" src="`
+    +recipe.image+`">\n<p class="modalInstructions">`+recipe.instruction+
+    `</p>\n`+tableText+`\n</div>\n<div class="modalBottom">\n<button  class="closeButton" onClick=Remove()>Close</button>\n</div>\n</div>\n<div class="overlay"></div>\n`;
+    htmlText = content;
+    var newEl = document.createElement("div");
+    var textNode = document.createElement("div");
+    textNode.innerHTML = htmlText;
+    newEl.appendChild(textNode);
+
+    var list = document.getElementsByTagName('body')[0];
+    list.insertBefore(newEl, list.childNodes[0]);
+}
+
+function Remove(){
+    var elem = document.getElementsByTagName("div")[0];
+    elem.remove();
 }
     
